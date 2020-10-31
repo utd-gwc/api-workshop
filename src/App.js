38bbc280354f2e5aa9e6b53fcb2823a995d71dc7
mustafa <mustafa.sadriwala@utdallas.edu>
2020-10-30 20:17:13 -0500

    abstracted parsing

--- src/App.js
+++ src/App.js
@@ -5,6 +5,32 @@ import Reps from './Reps';
 
 const API_KEY = process.env.REACT_APP_API_KEY
 
+function parser(offices, officials) {
+  var rows = [];
+  var currRow = [];
+  var officeIndex = 0;
+  const rowLength = 4;
+  while (officeIndex < offices.length) {
+    var currOffice = offices[officeIndex];
+    for (var i = 0; i < currOffice.officialIndices.length; i += 1) {
+      currRow.push({
+        "info": officials[currOffice.officialIndices[i]],
+        "office": currOffice
+      });
+      if (currRow.length === rowLength) {
+        rows.push(currRow);
+        currRow = [];
+      }
+    }
+    officeIndex += 1;
+  }
+
+  if (currRow.length > 0) {
+    rows.push(currRow);
+  }
+  return rows;
+}
+
 function App() {
   const [loading, setLoading] = useState(false);
   const [rowsOfReps, setRowsOfReps] = useState(null);
@@ -24,27 +50,7 @@ function App() {
     if (json.error != null) {
       alert("ERROR")
     } else {
-      var rows = [];
-      var currRow = [];
-      var officeIndex = 0;
-      const rowLength = 4;
-      while (officeIndex < json.offices.length) {
-        var currOffice = json.offices[officeIndex];
-        for (var i = 0; i < currOffice.officialIndices.length; i += 1) {
-          currRow.push({
-            "info": json.officials[currOffice.officialIndices[i]],
-            "office": currOffice
-          });
-          if (currRow.length === rowLength) {
-            rows.push(currRow);
-            currRow = [];
-          }
-        }
-        officeIndex += 1;
-      }
-      if (currRow.length > 0) {
-        rows.push(currRow);
-      }
+      const rows = parser(json.offices, json.officials)
       setRowsOfReps(rows);
     }
     setLoading(false);
